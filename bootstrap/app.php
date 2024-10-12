ae6045b94c5f6a9de87d805b33925fa4cf8d6b40
Muhamad Eko Alfianto <eko.alfian14@gmail.com>
2024-10-12 13:54:57 +0700

    register page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => AdminMiddleware::class,
+            'user' => UserMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/registration/form',[AuthController::class,'loadRegisterForm']);
+Route::post('/register/user',[AuthController::class,'registerUser'])->name('registerUser');
+
+
+Route::get('/404', [AuthController::class, 'load404']);
