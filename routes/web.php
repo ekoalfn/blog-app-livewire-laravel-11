ec2ad62122a0dd798c1c477f767240a3e5fd36e1
Muhamad Eko Alfianto <eko.alfian14@gmail.com>
2024-10-12 14:17:10 +0700

    login page

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/registration/form',[AuthController::class,'loadRegisterForm']);
+Route::get('/register',[AuthController::class,'loadRegisterForm']);
 Route::post('/register/user',[AuthController::class,'registerUser'])->name('registerUser');
 
+Route::get('/login/form',[AuthController::class,'loadLoginPage']);
+Route::post('/login/user',[AuthController::class,'LoginUser'])->name('LoginUser');
+Route::get('/logout',[AuthController::class,'LogoutUser']);
 
 Route::get('/404', [AuthController::class, 'load404']);
