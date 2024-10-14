6726d7c40f7731ff1b1f8996978703aaf8c3fd81
Muhamad Eko Alfianto <eko.alfian14@gmail.com>
2024-10-14 16:21:33 +0700

    post dashboard

--- app/Livewire/MyPosts.php
+++ app/Livewire/MyPosts.php
@@ -10,15 +10,19 @@ class MyPosts extends Component
 {
     public $my_posts;
 
+    public $my_posts_count;
+
     public function mount(){
         $user_id = Auth::user()->id;
         $this->my_posts = Post::where('user_id', $user_id)->get();
+        $this->my_posts_count = Post::where('user_id', $user_id)->count();
     }
 
     public function render()
     {
         return view('livewire.my-posts', [
-            'posts' => $this->my_posts
+            'posts' => $this->my_posts,
+            'post_count' => $this->my_posts_count
         ]);
     }
 }
--- resources/views/livewire/my-posts.blade.php
+++ resources/views/livewire/my-posts.blade.php
@@ -1,6 +1,58 @@
 <div class="card">
     <div class="card-body">
-      <table class="table datatable">
+        <h5 class="card-title"> My Dashboard</h5>
+
+        <div class="row mb-4" wire:poll>
+            <div class="col-md-4">
+                <div class="card info-card sales-card" style="background-color: #f0f8ff;">
+                    <div class="card-body">
+                        <h5 class="card-title text-primary">Followers</h5>
+                        <div class="d-flex align-items-center">
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center" style="background-color: #e0f7fa;">
+                                <i class="bi bi-people text-primary"></i>
+                            </div>
+                            <div class="ps-3">
+                                <h6 class="text-primary">145</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card info-card sales-card" style="background-color: #fff0f0;">
+                    <div class="card-body">
+                        <h5 class="card-title text-danger">Posts</h5>
+                        <div class="d-flex align-items-center">
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center" style="background-color: #ffe5e5;">
+                                <i class="bi bi-pencil text-danger"></i>
+                            </div>
+                            <div class="ps-3">
+                                <h6 class="text-danger">{{$post_count}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card info-card sales-card" style="background-color: #f0fff0;">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Comments</h5>
+                        <div class="d-flex align-items-center">
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center" style="background-color: #e5ffe5;">
+                                <i class="bi bi-chat-dots text-success"></i>
+                            </div>
+                            <div class="ps-3">
+                                <h6 class="text-success">145</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+          
+        <table class="table datatable">
         <thead>
           <tr>
             <th scope="col">#</th>
